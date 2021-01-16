f1d1a59a6e7121ea51a037a8b052db443dba4591
smaloney2017 <smaloney2017@my.fit.edu>
2021-01-16 13:19:27 -0500

    fixed toolbar close button

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -19,10 +19,10 @@ function Toolbar() {
         </div>
 
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul className='nav-menu-items' onClick={toggleSidebar}>
+          <ul className='nav-menu-items'>
             <li className='navbar-toggle'>
               <Link to='#' className='menu-bars'>
-                <AiIcons.AiOutlineClose />
+                <AiIcons.AiOutlineClose onClick={toggleSidebar}/>
               </Link>
             </li>
           </ul>

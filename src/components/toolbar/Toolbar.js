ddc0e3cc6ef1cd68642564d429865727a1a74292
smaloney2017 <smaloney2017@my.fit.edu>
2021-01-17 19:19:07 -0500

    simplified toolbar context

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -3,7 +3,6 @@ import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import './Toolbar.css';
-import { IconContext } from 'react-icons';
 
 function Toolbar() {
   const [sidebar, setSidebar] = useState(false);
@@ -11,24 +10,21 @@ function Toolbar() {
 
   return (
     <>
-      <IconContext.Provider value={{ color: '#fff' }}>
-        <div className='toolbar'>
-          <Link to='#' className='menu-bars'>
-            <FaIcons.FaBars onClick={toggleSidebar} />
-          </Link>
-        </div>
+      <div className='toolbar'>
+        <Link to='#' className='menu-bars'>
+          <FaIcons.FaBars onClick={toggleSidebar} />
+        </Link>
+      </div>
 
-        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-          <ul className='nav-menu-items'>
-            <li className='navbar-toggle'>
-              <Link to='#' className='menu-bars'>
-                <AiIcons.AiOutlineClose onClick={toggleSidebar}/>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-
-      </IconContext.Provider>
+      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className='nav-menu-items'>
+          <li className='navbar-toggle'>
+            <Link to='#' className='menu-bars'>
+              <AiIcons.AiOutlineClose onClick={toggleSidebar}/>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
   

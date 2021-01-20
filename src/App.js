62b1cb979703b8e3925850e61113649877e3cdcd
smaloney2017 <smaloney2017@my.fit.edu>
2021-01-20 18:44:43 -0500

    postgre instead of mysql

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Toolbar /><Survey />
         <header>React Map.</header>
         <div className='sub-header'>
-          Built by Kafele Wimbley and Sean Maloney using React.js, Node.js, MySQL, and Google Maps</div>
+          Built by Kafele Wimbley and Sean Maloney using React.js, Node.js, PostgreSQL, and Google Maps</div>
       </div>
     </>
   );

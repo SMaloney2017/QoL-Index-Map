0561bb9052f69b02d7475c278b3d11e617f71ac6
smaloney2017 <smaloney2017@my.fit.edu>
2021-05-26 23:32:12 -0400

    format display.js, change toolbar/header style

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-
-const style = {
-    width: '100%',
-    height: '100%'
-  };
 
 export class Display extends React.Component {
 
   render() {
     return (
       <>
-        <Map google={this.props.google} zoom={10}>
-            style={style}
-        </Map>
+
       </>
     );
   }
 }
-
-export default GoogleApiWrapper({
-    apiKey: (AIzaSyDXTz91zMUSd21UhHcVTrb1GPJbGMI4hko)
-  })(Display)

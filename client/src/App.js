f58cea48765ef369b0805863acbfa01a640178e6
smaloney2017 <smaloney2017@my.fit.edu>
2021-05-27 17:44:41 -0400

    added b/w color option

--- client/src/App.js
+++ client/src/App.js
@@ -20,9 +20,18 @@ class App extends React.Component {
             <br/>Built with React.js, Node.js, PostgreSQL, and Google Maps.
           </div>
           <div className = 'button-container'>
-            <button onClick = {() => this.changeColor('#6085ff')} className = 'header-buttons' style = {{backgroundColor: '#6085ff'}}></button>
-            <button onClick = {() => this.changeColor('#ff6060')} className = 'header-buttons' style = {{backgroundColor: '#ff6060'}}></button>
-            <button onClick = {() => this.changeColor('#00ff95')} className = 'header-buttons' style = {{backgroundColor: '#00ff95'}}></button>
+            <button onClick = {() => this.changeColor('#6085ff')} className = 'header-buttons' style = {{backgroundColor: '#6085ff70'}}>
+              <span className='header-buttons-front' style = {{backgroundColor: '#6085ff'}}></span>
+            </button>
+            <button onClick = {() => this.changeColor('#ff6060')} className = 'header-buttons' style = {{backgroundColor: '#ff606070'}}>
+              <span className='header-buttons-front' style = {{backgroundColor: '#ff6060'}}></span>
+            </button>
+            <button onClick = {() => this.changeColor('#00ff95')} className = 'header-buttons' style = {{backgroundColor: '#00ff9570'}}>
+              <span className='header-buttons-front' style = {{backgroundColor: '#00ff95'}}></span>
+            </button>
+            <button onClick = {() => this.changeColor('#575757')} className = 'header-buttons' style = {{backgroundColor: '#ffffff70'}}>
+              <span className='header-buttons-front-bw' style = {{backgroundColor: '#ffffff'}}></span>
+            </button>
           </div>
         </div>
       </>

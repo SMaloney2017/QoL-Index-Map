ca1d1ec8c3b7c1524724c2a9a33d5d6b1701063d
smaloney2017 <smaloney2017@my.fit.edu>
2021-10-04 19:48:33 -0400

    Added data visualization to Display.js

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import './Display.css'
+import { GoogleMap, LoadScript, Circle } from '@react-google-maps/api';
+import './Display.css';
 
 function formatData(queriedData) {
-  const displayData = []
-  const COORDS = []
+  var displayData = [{}]
+  var COORDS = []
   for(var i in queriedData){
     if (queriedData.hasOwnProperty(i)){
-      var obj = queriedData[i]
       COORDS[0] = queriedData[i].lat
       COORDS[1] = queriedData[i].lon
+      var gData = new window.google.maps.LatLng(COORDS[0], COORDS[1])
+      var obj = queriedData[i]
       var selectedOption = Object.keys(obj)[0];
       var weight = obj[selectedOption];
     }
-      displayData[i] = [COORDS, weight]
+      displayData[i] = {COORDS:gData, w: weight}
     }
     console.log(displayData)
     return displayData
  }
 
-
 function Display(props){
+  const displayData = formatData(props.getData)
+
   const containerStyle = {
     width: '100vw',
     height: '93.5vh'
@@ -30,6 +32,9 @@ function Display(props){
     lat: 28.56,
     lng: -80.64
   };
+
+  const colors = ["#ff2929", "#ff9429", "#ffff29", "#80ff5f", "#00ff95", "#1cccc8", "#3799fb"]
+
   return (
     <>
       <div>
@@ -41,7 +46,25 @@ function Display(props){
             center={defaultCenter}
             zoom={5}
           >
-            {/* <HeatmapLayer data={formatData(props.getData)} /> */}
+            {displayData.map(({COORDS, w }) => (
+              <Circle
+                center={COORDS}
+                options={{
+                  strokeColor: colors[w],
+                  strokeOpacity: .75,
+                  strokeWeight: 1,
+                  fillColor: colors[w],
+                  fillOpacity: 0.5,
+                  clickable: false,
+                  draggable: false,
+                  editable: false,
+                  visible: true,
+                  radius: 30000,
+                  zIndex: 1
+                }}
+              >
+              </Circle>
+            ))}
           </GoogleMap>
         </LoadScript>
       </div>

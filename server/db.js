1dcda34c76762e7c5ae18f62b6d28e66cc180812
smaloney2017 <smaloney2017@my.fit.edu>
2021-10-14 18:05:48 -0400

    Fixed db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "@reactmap_2021",
+    password: "$reactmap_2021",
     host: "localhost",
     port: 5432,
     database: "reactmap"

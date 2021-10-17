940be06018eca1d4130d05a67901290fd79325c7
smaloney2017 <smaloney2017@my.fit.edu>
2021-10-16 21:30:03 -0400

    Analysis component design

--- client/src/components/Analysis.js
+++ client/src/components/Analysis.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { AiFillCalculator } from 'react-icons/ai'
+import { AiOutlineCloseCircle, AiOutlineCheckCircle, AiFillCalculator } from 'react-icons/ai'
 import { FaMapMarkerAlt } from 'react-icons/fa'
 import { RiFolderChartLine, RiLineChartLine } from 'react-icons/ri'
 import './Analysis.css'
@@ -16,7 +16,17 @@ function Analysis(props) {
       <div className='analysis-container'>
         <div className={view ? 'analysis-button': 'analysis-screen'}>
           <span className={view ? 'analysis-button-front': 'analysis-screen-front'}>
-            <div className='analysis-icon'><AiFillCalculator onClick={toggleClass}/></div>
+            <div className={view ? 'analysis-icon': 'analysis-icon-active'}>
+              <div className={view ? 'analysis-icon-front': 'analysis-icon-front-active'}>
+                <div className={view ? null: 'hidden'}>
+                  <AiFillCalculator onClick={toggleClass} />
+                </div>
+                <div className={view ? 'hidden': 'icon-open'}>
+                  < AiOutlineCloseCircle onClick={toggleClass} />
+                </div>
+              </div>
+            </div>
+          
             <div className={view ? 'radius-hidden': 'radius-button'}><FaMapMarkerAlt onClick={toggleClass}/></div>
             <div className={view ? 'stats-hidden': 'stats-button'}><RiFolderChartLine onClick={toggleClass}/></div>
             <div className={view ? 'graphs-hidden': 'graphs-button'}><RiLineChartLine onClick={toggleClass}/></div>
